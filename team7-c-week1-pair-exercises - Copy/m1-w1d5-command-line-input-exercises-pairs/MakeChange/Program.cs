9a9ff8582ccc161e07063a7fb070b4e9f9e87427
dirtbeans <38547248+dirtbeans@users.noreply.github.com>
2018-08-19 07:48:19 -0400

    Trying drag and drop vs gitcommand
    Start Project

---
+++ team7-c-week1-pair-exercises - Copy/m1-w1d5-command-line-input-exercises-pairs/MakeChange/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeChange
+{
+    class Program
+    {
+        /*
+        Write a command line program which prompts the user for the total bill, and the amount tendered. 
+        It should then display the change required.
+ 
+        C:\Users> MakeChange
+         
+        Please enter the amount of the bill: 23.65
+        Please enter the amount tendered: 100.00
+        The change required is 76.35
+        */
+        static void Main(string[] args)
+        {
+
+            Console.Write("Please enter the amount of the bill: $");
+            string billAmount = Console.ReadLine();
+            double bill = double.Parse(billAmount);
+
+            Console.Write("Please enter the amount tendered: $");
+            string amountTendered =Console.ReadLine();
+            double payment = double.Parse(amountTendered);
+
+            double difference = (payment - bill);
+            if (difference<0)
+            Console.WriteLine("Please enter a valid amount. ");
+            else
+            Console.WriteLine("The change required : $" + difference);
+
+            
+
+        }
+    }
+}
---
+++ team7-c-week1-pair-exercises - Copy/m1-w1d5-command-line-input-exercises-pairs/MartianWeight/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartianWeight
+{
+    class Program
+    {
+        /*
+        In case you've ever pondered how much you weight on Mars, here's the calculation:
+ 	    Wm = We* 0.378
+ 	    where 'Wm' is the weight on Mars, and 'We' is the weight on Earth
+
+        Write a command line program which accepts a series of Earth weights from the user  
+        and displays each Earth weight as itself, and its Martian equivalent.
+
+
+        C:\Users> MartianWeight  
+        Enter a series of Earth weights (space-separated): 98 235 185
+            
+        98 lbs.on Earth, is 37 lbs.on Mars.
+        235 lbs.on Earth, is 88 lbs.on Mars.
+        185 lbs.on Earth, is 69 lbs.on Mars. 
+        */
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a series of Earth weights (space-separated): ");
+            string earthWeights = Console.ReadLine();
+            string[] splitEarthWeights = earthWeights.Split(' ');
+            double marsWeight1 = CalculateMarsWeight(splitEarthWeights[0]);
+            double marsWeight2 = CalculateMarsWeight(splitEarthWeights[1]);
+            double marsWeight3 = CalculateMarsWeight(splitEarthWeights[2]);
+            Console.WriteLine(splitEarthWeights[0] + " lbs. on Earth, is " + marsWeight1 +" lbs. on Mars.");
+            Console.WriteLine(splitEarthWeights[1] + " lbs. on Earth, is " + marsWeight2 + " lbs. on Mars.");
+            Console.WriteLine(splitEarthWeights[2] + " lbs. on Earth, is " + marsWeight3 + " lbs. on Mars.");
+            Console.ReadLine();
+        }
+
+        static double CalculateMarsWeight(string earthWeight)
+        {
+            int earthWeightParsed = int.Parse(earthWeight);
+            return earthWeightParsed * .378;
+
+        }
+    }
+}
